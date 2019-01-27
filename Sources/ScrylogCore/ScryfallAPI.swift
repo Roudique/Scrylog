2f2b498e12613abbd8747adecfe759b0808fb09a
Roudique <roudique@gmail.com>
2019-01-27 02:22:49 -0500

    Add ScryfallAPI

--- Sources/ScrylogCore/ScryfallAPI.swift
+++ Sources/ScrylogCore/ScryfallAPI.swift
@@ -6,9 +6,53 @@
 //
 
 import Foundation
+import ScryLogHTMLParser
 
 public class ScryfallAPI {
-    public static func fetchLatestDocuments(completion: ([Data]?) -> Void) {
-        
+    private static let links = ["https://scryfall.com/docs/api/errors",
+                                "https://scryfall.com/docs/api/lists",
+                                "https://scryfall.com/docs/api/sets",
+                                "https://scryfall.com/docs/api/cards",
+                                "https://scryfall.com/docs/api/rulings",
+                                "https://scryfall.com/docs/api/card-symbols",
+                                "https://scryfall.com/docs/api/catalogs"
+    ]
+
+}
+
+public extension ScryfallAPI {
+    static func fetchLatestDocuments(completion: @escaping ([String: [Table]]?) -> Void) {
+        let tablesDict = [String: [Table]]()
+
+        fetchDocument(index: 0, tablesDict: tablesDict, completion: completion)
     }
+}
+
+private extension ScryfallAPI {
+    static func fetchDocument(index: Int,
+                              tablesDict: [String: [Table]],
+                              completion: @escaping ([String: [Table]]?) -> Void) {
+        let url = URL(string: self.links[index])!
+        let dataTask = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            let tables = ScryLogHTMLParser.parse(data: data)
+            let title = url.lastPathComponent
+            
+            var newTablesDict = tablesDict
+            newTablesDict[title] = tables
+            let newIndex = index + 1
+            
+            if newIndex == self.links.count {
+                completion(newTablesDict)
+            } else {
+                fetchDocument(index: newIndex, tablesDict: newTablesDict, completion: completion)
+            }
+        }
+        dataTask.resume()
+    }
+
 }
